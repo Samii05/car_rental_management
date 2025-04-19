f8d08b8d3793884ad2b6eed0add538c209bf13b2
Samii05 <sami_ramzi.rezig@g.enp.edu.dz>
2025-04-19 23:58:53 +0200

    Update connectdb.php

--- pages&connectiondb/connectdb.php
+++ pages&connectiondb/connectdb.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "car";
-try {
+try {                                            
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
